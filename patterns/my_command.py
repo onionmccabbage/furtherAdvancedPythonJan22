29a009dff8ad048ab5fed621f02fad3736bce65e
onionmccabbage <onionmccabbage@live.com>
2022-01-24 15:42:06 +0000

    Add files via upload

---
+++ patterns/my_command.py
@@ -0,0 +1,44 @@
+# using the command design pattern
+from abc import ABCMeta, abstractmethod
+
+class Order(metaclass=ABCMeta):
+    @abstractmethod
+    def execute(self):
+        pass
+
+class StockTrade():
+    def buy(self):
+        print('buy stocks')
+    def sell(self):
+        print('sell stocks')
+
+class Agent():
+    def __init__(self):
+        self.__orderQueue = [] # start with an empty list
+    def placeOrder(self, order):
+        self.__orderQueue.append(order)
+        order.execute() # we could wait until resources become available
+
+# concrete orders
+class BuyStock(Order):
+    def __init__(self, stock):
+        self.stock = stock
+    def execute(self):
+        return self.stock.buy()
+
+class SellStock(Order):
+    def __init__(self, stock):
+        self.stock = stock
+    def execute(self):
+        return self.stock.sell()
+
+if __name__ == '__main__':
+    # client
+    stock = StockTrade()
+    buy   = BuyStock(stock)
+    sell  = SellStock(stock)
+    # invoker
+    agent = Agent()
+    # we can now invoke the commands to buy or sell
+    agent.placeOrder(buy)
+    agent.placeOrder(sell)
